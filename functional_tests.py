01487fc4201f8fa5570230b8dde65e4f36bd1ff8
ItaloSSilva19 <iti.s.silva@gmail.com>
2020-10-21 13:17:40 -0300

    FT Test if there is a h1 tag

--- functional_tests.py
+++ functional_tests.py
@@ -15,27 +15,29 @@ class HomePageTest(unittest.TestCase):
         
         #It sees the title and header mention Todo lists
         self.assertIn('To-Do', self.browser.title)
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header.text)
 
-# It is invited to enter a to-do item straight away
+    # It is invited to enter a to-do item straight away
+        self.fail('finish this test')
+    # It types "Buy peacock featitss" into a text box (Peba's hobby
+    # is tying fly-fishing lures)
 
-# It types "Buy peacock featitss" into a text box (Peba's hobby
-# is tying fly-fishing lures)
+    # When it hits enter, the page updates, and now the page lists
+    # "1: Buy peacock featitss" as an item in a to-do list
 
-# When it hits enter, the page updates, and now the page lists
-# "1: Buy peacock featitss" as an item in a to-do list
+    # There is still a text box inviting its to add another item. It
+    # enters "Use peacock feathers to make a fly" (Peba is very methodical)
 
-# There is still a text box inviting its to add another item. It
-# enters "Use peacock feathers to make a fly" (Peba is very methodical)
+    # The page updates again, and now shows both items on its list
 
-# The page updates again, and now shows both items on its list
+    # Peba wonders whether the site will remember its list. Then it sees
+    # that the site has generated a unique URL for its -- there is some
+    # explanatory text to that effect.
 
-# Peba wonders whether the site will remember its list. Then it sees
-# that the site has generated a unique URL for its -- there is some
-# explanatory text to that effect.
+    # It visits that URL - its to-do list is still there.
 
-# It visits that URL - its to-do list is still there.
-
-# Satisfied, it goes back to sleep
-        
+    # Satisfied, it goes back to sleep
+            
 if __name__ == '__main__':
     unittest.main()
