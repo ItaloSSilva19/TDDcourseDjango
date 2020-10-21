1e708d33ba82aa0d03322ce1650f5f8fb61c5848
ItaloSSilva19 <iti.s.silva@gmail.com>
2020-10-21 14:36:02 -0300

    refactor unitTest

--- lists/tests.py
+++ lists/tests.py
@@ -6,11 +6,5 @@ class HomePageTest(TestCase):
 
     def test_home_page(self):
         response = self.client.get('/')
-        self.assertIn(
-            '<title>To-Do lists</title>',
-            response.content.decode()
-        )
-        print(response.content)
-        self.assertTrue(response.content.decode().startswith('<html>'))
-        self.assertTrue(response.content.decode().endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
     
