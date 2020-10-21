6d82e7c804350071c546da5c924c801a43b80e67
ItaloSSilva19 <iti.s.silva@gmail.com>
2020-10-21 13:19:06 -0300

    UT(unitTest) tests whether the homepage is accessible and if the title is To-Do lists

--- lists/tests.py
+++ lists/tests.py
@@ -4,5 +4,12 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-    def test_maths_works(self):
-        self.assertEqual(1 + 1, 3)
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertIn(
+            '<title>To-Do lists</title>',
+            response.content.decode()
+        )
+        self.assertTrue(response.content.decode().startswith('<html>'))
+        self.assertTrue(response.content.decode().endswith('</html>'))
+    
