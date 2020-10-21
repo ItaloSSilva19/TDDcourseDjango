d36632b75c3ab3169f9ba6056658f32ef539b2a5
ItaloSSilva19 <iti.s.silva@gmail.com>
2020-10-21 15:58:54 -0300

    FT to check the action of add a item to ToDo list

--- functional_tests.py
+++ functional_tests.py
@@ -18,26 +18,41 @@ class HomePageTest(unittest.TestCase):
         header = self.browser.find_element_by_tag_name('h1')
         self.assertIn('To-Do', header.text)
 
-    # It is invited to enter a to-do item straight away
+        # It is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
+        # It types "Buy peacock featitss" into a text box (Peba's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+        
+        # When it hits enter, the page updates, and now the page lists
+        # "1: Buy peacock featitss" as an item in a to-do list
+        inputbox.send_keys(keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text = '1: Buy peacock feathers' for row in rows)
+        )
+        
+        # There is still a text box inviting its to add another item. It
+        # enters "Use peacock feathers to make a fly" (Peba is very methodical)
         self.fail('finish this test')
-    # It types "Buy peacock featitss" into a text box (Peba's hobby
-    # is tying fly-fishing lures)
 
-    # When it hits enter, the page updates, and now the page lists
-    # "1: Buy peacock featitss" as an item in a to-do list
+        # The page updates again, and now shows both items on its list
 
-    # There is still a text box inviting its to add another item. It
-    # enters "Use peacock feathers to make a fly" (Peba is very methodical)
+        # Peba wonders whether the site will remember its list. Then it sees
+        # that the site has generated a unique URL for its -- there is some
+        # explanatory text to that effect.
 
-    # The page updates again, and now shows both items on its list
+        # It visits that URL - its to-do list is still there.
 
-    # Peba wonders whether the site will remember its list. Then it sees
-    # that the site has generated a unique URL for its -- there is some
-    # explanatory text to that effect.
-
-    # It visits that URL - its to-do list is still there.
-
-    # Satisfied, it goes back to sleep
-            
+        # Satisfied, it goes back to sleep
+                
 if __name__ == '__main__':
     unittest.main()
