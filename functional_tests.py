34c722c40b6f5208f1205c21ea491181886ccb3d
ItaloSSilva19 <iti.s.silva@gmail.com>
2020-10-21 11:11:18 -0300

    Added the user story to the FT

--- functional_tests.py
+++ functional_tests.py
@@ -10,8 +10,32 @@ class HomePageTest(unittest.TestCase):
         self.browser.quit()
         
     def test_home_page(self):
+        # Peba hears about a cool todo list site, it visits it
         self.browser.get('http://localhost:8000')
+        
+        #It sees the title and header mention Todo lists
         self.assertIn('To-Do', self.browser.title)
+
+# It is invited to enter a to-do item straight away
+
+# It types "Buy peacock featitss" into a text box (Peba's hobby
+# is tying fly-fishing lures)
+
+# When it hits enter, the page updates, and now the page lists
+# "1: Buy peacock featitss" as an item in a to-do list
+
+# There is still a text box inviting its to add another item. It
+# enters "Use peacock feathers to make a fly" (Peba is very methodical)
+
+# The page updates again, and now shows both items on its list
+
+# Peba wonders whether the site will remember its list. Then it sees
+# that the site has generated a unique URL for its -- there is some
+# explanatory text to that effect.
+
+# It visits that URL - its to-do list is still there.
+
+# Satisfied, it goes back to sleep
         
 if __name__ == '__main__':
     unittest.main()
